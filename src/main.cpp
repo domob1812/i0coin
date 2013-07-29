0f07d326c1089c4190b7009d9e045f8c1f9ac47b
Rik Snel <rik@snel.it>
2013-07-29 13:06:25 +0200

    After merge, the subsidy halving block was wrong.
    This patch corrects it.

--- src/main.cpp
+++ src/main.cpp
@@ -868,8 +868,8 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
 {
     int64 nSubsidy = 48 * COIN;
 
-    // Subsidy is cut in half every 210000 blocks, which will occur approximately every 4 years
-    nSubsidy >>= (nHeight / 210000);
+    // Subsidy is cut in half every 218750 blocks, which will occur approximately every 4 years
+    nSubsidy >>= (nHeight / 218750);
 
     return nSubsidy + nFees;
 }
