470c5835a70bfc43bdb9f88cb07a5d369d228528
Rik Snel <rik@snel.it>
2013-08-14 11:30:11 +0200

    Hardforking release candidate: 0.8.3-2rc1

--- contrib/verifysfbinaries/verify.sh
+++ contrib/verifysfbinaries/verify.sh
@@ -18,7 +18,7 @@ WORKINGDIR="/tmp/bitcoin"
 TMPFILE="hashes.tmp"
 
 #this URL is used if a version number is not specified as an argument to the script
-SIGNATUREFILE="http://i0coin.snel.it/i0coin-0.8.3-1/SHA256SUMS.asc"
+SIGNATUREFILE="http://i0coin.snel.it/i0coin-0.8.3-2rc1/SHA256SUMS.asc"
 
 SIGNATUREFILENAME="SHA256SUMS.asc"
 RCSUBDIR="test/"
--- doc/README_windows.txt
+++ doc/README_windows.txt
@@ -1,4 +1,4 @@
-I0coin 0.8.3-1 BETA
+I0coin 0.8.3-2rc1 BETA
 
 Copyright (c) 2009-2013 Bitcoin and I0coin Developers
 Distributed under the MIT/X11 software license, see the accompanying
