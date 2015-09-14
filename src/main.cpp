8f7de7604ab0683e9132eb99a923250ec783d3d3
Daniel Kraft <d@domob.eu>
2015-09-14 11:38:36 +0200

    Bump version to 0.8.6.1 and also change protocol version.
    Use new protocol version 80601 to signify the change with 2000 -> 1000
    headers per "headers" message.

--- src/main.cpp
+++ src/main.cpp
@@ -3710,7 +3710,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
 
         // we must use CBlocks, as CBlockHeaders won't include the 0x00 nTx count at the end
         vector<CBlock> vHeaders;
-        int nLimit = 2000;
+        int nLimit = 1000;
         printf("getheaders %d to %s\n", (pindex ? pindex->nHeight : -1), hashStop.ToString().c_str());
         for (; pindex; pindex = pindex->pnext)
         {
